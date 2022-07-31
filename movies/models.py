fd190752f660e98ef2d8bb4d2aeefd2b38b675ab
onyangodonomondi <onyangodonomondi@gmail.com>
2022-08-01 00:34:35 +0300

    first commit

---
+++ movies/models.py
@@ -0,0 +1,21 @@
+from email.policy import default
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class Genre (models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    number_in_stock = models.IntegerField()
+    daily_rates = models.FloatField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now)
